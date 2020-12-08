9c4158169b83402dbfa1c1ccca9543ddbd6dd7b4
Bot <ms-devexp@redhat.com>
2020-12-08 11:11:45 +0000

    Update to ce55690

---
+++ 968.bundle.js
@@ -0,0 +1,6 @@
+/*!
+ * Copyright Strimzi authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ *
+ */
+(self.webpackChunkstrimzi_ui=self.webpackChunkstrimzi_ui||[]).push([[968],{172:(e,i,t)=>{"use strict";t.r(i),t.d(i,{Home:()=>Home});var r=t(422),c=t.n(r);const a=t.p+"images/ee164d9677246826ea6c71c2f2f12416.png";var Home=function(e){var i=e.children;return c().createElement("div",{className:"home"},c().createElement("img",{src:a,alt:"Strimzi logo"}),"Welcome to the Strimzi UI",i)}}}]);
